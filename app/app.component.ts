c7ef4aecf7f15fb7e9842f5c44c89ec9d7038fbf
prabhu-raja <prabhuraja@gmail.com>
2018-12-15 15:02:25 -0500

    Bootsrapping Angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  styleUrls:['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      {{title}}
     </div>
   `
 })
 export class AppComponent {
   title: string;
-  constructor() {
-    this.title = 'Ultimate Angular';
-  }
+   constructor() {
+     this.title = 'My Title🐝';
+   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     CommonModule
   ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
