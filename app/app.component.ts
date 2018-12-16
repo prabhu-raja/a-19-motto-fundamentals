dc10c15598601c3553170527ef069a717f75f8a5
prabhu-raja <prabhuraja@gmail.com>
2018-12-15 21:46:43 -0500

    implemnted property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-root',
   styleUrls:['app.component.scss'],
-  template: `
-    <div class="app">
-      {{title}}
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title: string;
+  name: string = 'Jack'
    constructor() {
      this.title = 'My Title🐝';
    }
