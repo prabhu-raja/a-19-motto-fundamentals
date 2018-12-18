d57184b0399a9309a06e86ad60ea1e9bfa5a11b1
prabhu-raja <prabhuraja@gmail.com>
2018-12-17 22:09:19 -0500

    ngFor and template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
 @Component({
   selector: 'app-root',
   styleUrls:['app.component.scss'],
@@ -7,9 +12,25 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 
-  name: string = '';
-
-  handleChange(val: string) {
-    this.name = val;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
