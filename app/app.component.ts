146ff4a925732ed555caaf97464613f58a2cd999
prabhu-raja <prabhuraja@gmail.com>
2018-12-18 21:12:07 -0500

    Pipe and safe navigation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
 
+interface Child {
+  name: string,
+  age: number
+}
 interface Passenger {
   id: number,
   fullname: string,
   checkedIn: boolean,
-  isVip: boolean
+  checkInDate: number | null,
+  children: Child[] | null
 }
 @Component({
   selector: 'app-root',
@@ -17,26 +22,31 @@ export class AppComponent {
     id: 1,
     fullname: 'Stephen',
     checkedIn: true,
-    isVip: false
+    checkInDate: 1490742000000,
+    children: null
   }, {
     id: 2,
     fullname: 'Rose',
     checkedIn: false,
-    isVip: false
+    checkInDate: null,
+    children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
   }, {
     id: 3,
     fullname: 'James',
     checkedIn: true,
-    isVip: true
+    checkInDate: 1491606000000,
+    children: null
   }, {
     id: 4,
     fullname: 'Louise',
     checkedIn: true,
-    isVip: false
+    checkInDate: 1488412800000,
+    children: [{ name: 'Jessica', age: 1 }]
   }, {
     id: 5,
     fullname: 'Tina',
     checkedIn: false,
-    isVip: true
+    checkInDate: null,
+    children: null
   }];
 }
