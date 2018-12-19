74cdb46b313538a61fe2511ee673be0e8e23e940
prabhu-raja <prabhuraja@gmail.com>
2018-12-18 23:25:14 -0500

    ngOnit created

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,46 +1,56 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
 @Component({
     selector: 'passenger-dashboard',
     templateUrl: './passenger-dashboard.component.html',
     styleUrls: ['passenger-dashboard.component.scss']
 })
-export class PassengerDashboardComponent {
-    passengers: Passenger[] = [
-        {
-            id: 1,
-            fullname: 'Stephen',
-            checkedIn: true,
-            checkInDate: 1490742000000,
-            children: null
-        }, 
-        {
-            id: 2,
-            fullname: 'Rose',
-            checkedIn: false,
-            checkInDate: null,
-            children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-        }, 
-        {
-            id: 3,
-            fullname: 'James',
-            checkedIn: true,
-            checkInDate: 1491606000000,
-            children: null
-        }, 
-        {
-            id: 4,
-            fullname: 'Louise',
-            checkedIn: true,
-            checkInDate: 1488412800000,
-            children: [{ name: 'Jessica', age: 1 }]
-        }, 
-        {
-            id: 5,
-            fullname: 'Tina',
-            checkedIn: false,
-            checkInDate: null,
-            children: null
-        }
-    ];
+export class PassengerDashboardComponent implements OnInit {
+    passengers: Passenger[];
+
+    constructor() {
+        
+    }
+    
+    ngOnInit() {
+        this.passengers = [
+            {
+                id: 1,
+                fullname: 'Stephen',
+                checkedIn: true,
+                checkInDate: 1490742000000,
+                children: null
+            }, 
+            {
+                id: 2,
+                fullname: 'Rose',
+                checkedIn: false,
+                checkInDate: null,
+                children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
+            }, 
+            {
+                id: 3,
+                fullname: 'James',
+                checkedIn: true,
+                checkInDate: 1491606000000,
+                children: null
+            }, 
+            {
+                id: 4,
+                fullname: 'Louise',
+                checkedIn: true,
+                checkInDate: 1488412800000,
+                children: [{ name: 'Jessica', age: 1 }]
+            }, 
+            {
+                id: 5,
+                fullname: 'Tina',
+                checkedIn: false,
+                checkInDate: null,
+                children: null
+            }
+        ];
+    }
+
+    
 }
