1c632d0fa9a40324f6af29e1826376d721e83a12
prabhu-raja <prabhuraja@gmail.com>
2018-12-18 23:13:04 -0500

    created passenger dashboard module and component, exported to app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,6 @@
 import { Component } from "@angular/core";
 
-interface Child {
-  name: string,
-  age: number
-}
-interface Passenger {
-  id: number,
-  fullname: string,
-  checkedIn: boolean,
-  checkInDate: number | null,
-  children: Child[] | null
-}
+
 @Component({
   selector: 'app-root',
   styleUrls:['app.component.scss'],
@@ -18,35 +8,5 @@ interface Passenger {
 })
 export class AppComponent {
 
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkInDate: 1490742000000,
-    children: null
-  }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false,
-    checkInDate: null,
-    children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-  }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true,
-    checkInDate: 1491606000000,
-    children: null
-  }, {
-    id: 4,
-    fullname: 'Louise',
-    checkedIn: true,
-    checkInDate: 1488412800000,
-    children: [{ name: 'Jessica', age: 1 }]
-  }, {
-    id: 5,
-    fullname: 'Tina',
-    checkedIn: false,
-    checkInDate: null,
-    children: null
-  }];
+  
 }
