01526c39fd3e149edbc91ff4d54ba554dc0a34e9
prabhu-raja <prabhuraja@gmail.com>
2018-12-19 23:43:54 -0500

    Created presentational components

---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'passenger-detail',
+    templateUrl: './passenger-detail.component.html'
+})
+export class PassengerDetailComponent {
+
+}
