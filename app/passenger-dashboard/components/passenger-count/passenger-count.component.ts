534c29776575c8c00923b3e10497d083c1f0fbd0
prabhu-raja <prabhuraja@gmail.com>
2018-12-22 15:29:40 -0500

    Added Compdoc package for documentation 📊

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -8,7 +8,9 @@ import { Passenger } from './../../models/passenger.interface';
 export class PassengerCountComponent {
     @Input()
     items: Passenger[];
-
+    /**
+     * Return length from the Passangers List
+     */
     checkedInCount(): number {
         if(!this.items) return;
         return this.items.filter((itm: Passenger) => itm.checkedIn).length;
