bad5377e1183754d0379d8a6f961c0ea9d53c986
prabhu-raja <prabhuraja@gmail.com>
2018-12-22 20:16:02 -0500

    created a service ⚙️ for dashboard module

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
+import { PassengerDashboardService } from './../../services/passenger-dashboard.service';
 @Component({
     selector: 'passenger-dashboard',
     templateUrl: './passenger-dashboard.component.html',
@@ -8,48 +9,10 @@ import { Passenger } from "../../models/passenger.interface";
 export class PassengerDashboardComponent implements OnInit {
     passengers: Passenger[];
 
-    constructor() {
-        
-    }
+    constructor(private passengerDashboardService: PassengerDashboardService) { }
     
     ngOnInit() {
-        this.passengers = [
-            {
-                id: 1,
-                fullname: 'Stephen',
-                checkedIn: true,
-                checkInDate: 1490742000000,
-                children: null
-            }, 
-            {
-                id: 2,
-                fullname: 'Rose',
-                checkedIn: false,
-                checkInDate: null,
-                children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-            }, 
-            {
-                id: 3,
-                fullname: 'James',
-                checkedIn: true,
-                checkInDate: 1491606000000,
-                children: null
-            }, 
-            {
-                id: 4,
-                fullname: 'Louise',
-                checkedIn: true,
-                checkInDate: 1488412800000,
-                children: [{ name: 'Jessica', age: 1 }]
-            }, 
-            {
-                id: 5,
-                fullname: 'Tina',
-                checkedIn: false,
-                checkInDate: null,
-                children: null
-            }
-        ];
+        this.passengers = this.passengerDashboardService.getPassengers();
     }
 
     handleRemove(evt: Passenger) {
